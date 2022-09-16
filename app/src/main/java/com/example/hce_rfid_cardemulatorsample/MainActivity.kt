6ba15b7335c1abd844e0ce52c27abb8e3639253d
layonmartins <layon.fonseca@eldorado.org.br>
2022-09-16 15:52:14 -0300

    Update the apdu response

--- app/src/main/java/com/example/hce_rfid_cardemulatorsample/MainActivity.kt
+++ app/src/main/java/com/example/hce_rfid_cardemulatorsample/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.hce_rfid_cardemulatorsample
 
+import android.content.ComponentName
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.nfc.cardemulation.HostApduService
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.widget.TextView
@@ -21,9 +22,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.d("layon.f", "MainActivity - onCreate()")
         hostApduServiceIntent = Intent(this@MainActivity, MyHostApduService::class.java)
         checkIfDeviceCanEmulateHostNFCTag()
+        binding.editTextApduResponse.setText(Utils.APDURESPONSE)
+        setupApduTextChangedListener()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val pm = packageManager
+
+        pm.setComponentEnabledSetting(
+            ComponentName(this@MainActivity, "com.example.hce_rfid_cardemulatorsample.MyHostApduService"),
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            PackageManager.DONT_KILL_APP
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val pm = packageManager
+
+        pm.setComponentEnabledSetting(
+            ComponentName(this@MainActivity, "com.example.hce_rfid_cardemulatorsample.MyHostApduService"),
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
+    }
+
+    private fun setupApduTextChangedListener() {
+        binding.editTextApduResponse.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                Utils.APDURESPONSE = s.toString()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
     }
 
     fun startBlinkEffect(text : TextView) {
@@ -43,26 +81,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkIfDeviceCanEmulateHostNFCTag(){
         if(packageManager.hasSystemFeature(PackageManager.FEATURE_NFC_HOST_CARD_EMULATION)){
-            setupButtonsClickListenter()
-            binding.buttonStart.performClick()
+            startBlinkEffect(binding.emulating)
         } else {
             binding.emulating.text = "Your device can NOT emulate NFC cards"
-        }
-    }
-
-    private fun setupButtonsClickListenter() {
-        binding.apply {
-            buttonStart.setOnClickListener {
-                Log.d("layon.f", "MainActivity - buttonStart clicked")
-                startService(hostApduServiceIntent)
-                startBlinkEffect(binding.emulating)
-            }
-
-            buttonStop.setOnClickListener {
-                Log.d("layon.f", "MainActivity - buttonStop clicked")
-                stopService(hostApduServiceIntent)
-                stopBlinkEffect(binding.emulating)
-            }
         }
     }
 
--- app/src/main/java/com/example/hce_rfid_cardemulatorsample/MyHostApduService.kt
+++ app/src/main/java/com/example/hce_rfid_cardemulatorsample/MyHostApduService.kt
@@ -12,14 +12,36 @@ class MyHostApduService : HostApduService() {
     override fun processCommandApdu(commandApdu: ByteArray, extras: Bundle?): ByteArray {
         Log.d("layon.f", "processCommandApdu(commandApdu = $commandApdu, extras = $extras)")
 
-        return commandApdu
+        if (commandApdu == null) {
+            return Utils.hexStringToByteArray(STATUS_FAILED)
+        }
+
+        val hexCommandApdu = Utils.toHex(commandApdu)
+        if (hexCommandApdu.length < MIN_APDU_LENGTH) {
+            return Utils.hexStringToByteArray(STATUS_FAILED)
+        }
+
+        if (hexCommandApdu.substring(0, 2) != DEFAULT_CLA) {
+            return Utils.hexStringToByteArray(CLA_NOT_SUPPORTED)
+        }
+
+        if (hexCommandApdu.substring(2, 4) != SELECT_INS) {
+            return Utils.hexStringToByteArray(INS_NOT_SUPPORTED)
+        }
+
+        if (hexCommandApdu.substring(10, 24) == AID)  {
+            return Utils.APDURESPONSE.toByteArray()
+        } else {
+            return Utils.hexStringToByteArray(STATUS_FAILED)
+        }
+
     }
 
     // The `onDeactiveted` method will be called when the a different
     // AID has been selected or the NFC connection has been lost
     override fun onDeactivated(reason: Int) {
         Log.d(
-            "layon.f", "onDeactivated(reason = ${
+            TAG, "onDeactivated(reason = ${
                 when (reason) {
                     0 -> "DEACTIVATION_LINK_LOSS"
                     1 -> "DEACTIVATION_DESELECTED"
@@ -29,21 +51,31 @@ class MyHostApduService : HostApduService() {
         )
     }
 
-
-
     override fun onCreate() {
         super.onCreate()
-        Log.d("layon.f", "MyHostApduService onCreate()")
+        Log.d(TAG, "MyHostApduService onCreate()")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d("layon.f", "onStartCommand intent: $intent, flags: $flags, startId: $startId")
+        Log.d(TAG, "onStartCommand intent: $intent, flags: $flags, startId: $startId")
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("layon.f", "MyHostApduService onDestroy()")
+        Log.d(TAG, "MyHostApduService onDestroy()")
+    }
+
+    companion object {
+        val TAG = "layon.f"
+        val STATUS_SUCCESS = "9000"
+        val STATUS_FAILED = "6F00"
+        val CLA_NOT_SUPPORTED = "6E00"
+        val INS_NOT_SUPPORTED = "6D00"
+        val AID = "F0010203040506"
+        val SELECT_INS = "A4"
+        val DEFAULT_CLA = "00"
+        val MIN_APDU_LENGTH = 12
     }
 
 }
